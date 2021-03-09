be10c729117d4ff42407431303971037d4ddf335
A-Silkwood <silkwood.andrew@gmailcom>
2021-03-09 16:52:28 -0700

    Added in functionality to acknowledge and handle messages.

--- client.cpp
+++ client.cpp
@@ -1,4 +1,69 @@
-//
-// Created by silkwood on 3/3/21.
-//
+#include <iostream>      /* for printf() and fprintf() */
+#include <cstdlib>     /* for atoi() and exit() */
+#include <cstring>     /* for memset() */
+#include <unistd.h>     /* for close() */
 
+#include <sys/socket.h> /* for socket(), connect(), sendto(), and recvfrom() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+
+// print error and exits
+void DieWithError(const char *errorMessage) {
+    perror(errorMessage);
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    const int RCVMAX = 1400;
+
+    int sock;                       // socket
+    struct sockaddr_in serverAddr;  // server address
+    unsigned short csPort;          // client-server port
+    char *serverIP;                 // server IP address
+
+    struct sockaddr_in clientAddr;  // client address
+    unsigned int clientAddrLen;     // client address length
+    char buffer[RCVMAX+1];          // message buffer
+    int respStringLen;
+
+    // check format
+    if(argc != 3) {
+        fprintf(stderr, "Usage: %s <SERVER IP ADDRESS> <SERVER PORT>\n", argv[0]);
+        exit(1);
+    }
+
+    serverIP = argv[1]; // receive server IP
+    csPort = atoi(argv[2]);   // receive client-server port
+
+    printf("Connecting to %s, port %d\n", serverIP, csPort);
+
+    // create socket
+    if((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+        DieWithError("socket() failed");
+    }
+
+    // construct server address structure
+    memset(&serverAddr, 0, sizeof(serverAddr));      // zero out structure
+    serverAddr.sin_family = AF_INET;                    // address family
+    serverAddr.sin_addr.s_addr = inet_addr(serverIP);   // server IP address
+    serverAddr.sin_port = htons(csPort);                // port
+
+    // client side loop
+    while(buffer != "EXT") {
+        printf("Enter message:\n");
+        std::cin >> buffer;
+
+        if(sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) != strlen(buffer)) {
+            DieWithError("sendto() sent a different number of bytes than expected");
+        }
+
+        clientAddrLen = sizeof(clientAddr);
+
+        if((respStringLen = recvfrom(sock, buffer, RCVMAX, 0, (struct sockaddr*) &clientAddr, &clientAddrLen)) > RCVMAX) {
+            DieWithError("recvfrom() failed");
+        }
+    }
+
+    // close client
+    close(sock);
+    exit(0);
+}
--- server.cpp
+++ server.cpp
@@ -2,27 +2,79 @@
 #include <cstdlib>     /* for atoi() and exit() */
 #include <cstring>     /* for memset() */
 #include <unistd.h>     /* for close() */
+#include <vector>
 
 #include <sys/socket.h> /* for socket(), connect(), sendto(), and recvfrom() */
 #include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
 
+struct message {
+    int lastMsg = 0;
+    std::string addr;
+    std::string msg;
+};
+
 // print error and exits
-void DieWithError(const char *errorMessage) {
+void DieWithError(const char* errorMessage) {
     perror(errorMessage);
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
-    const int RECVMAX = 255;        // longest message to receive
+// acknowledges client
+void acknowledge(int sock, std::string* message, int msgNum, struct sockaddr_in* clientAddr) {
+    // build acknowledgement message
+    char* ackMsg;
+    sprintf(ackMsg, "ACK%d:%s", msgNum, message);
 
+    // send acknowledgement
+    if(sendto(sock, ackMsg, strlen(ackMsg), 0, (struct sockaddr*) clientAddr, sizeof(*clientAddr)) != strlen(ackMsg)) {
+        DieWithError("sendto() sent a different number of bytes than expected");
+    }
+}
+
+// finds message being built in msgs
+struct message* getMessage(std::vector<message*>* msgs, std::string* ip) {
+    for(int i = 0; i < msgs->size(); i++) {
+        if((*msgs)[i]->addr == *ip) {
+            return (*msgs)[i];
+        }
+    }
+    return NULL;
+}
+
+// handles receiving messages
+void handleMessage(int sock, std::vector<message*>* msgs, struct sockaddr_in* clientAddr, int msgNum, std::string* content) {
+    std::string addr = std::string(inet_ntoa((*clientAddr).sin_addr));
+    struct message* msg = getMessage(msgs, &addr);
+    if(msg == NULL) {
+        // create message
+        message* newMsg = new message;
+        newMsg->addr = std::string(inet_ntoa((*clientAddr).sin_addr));
+        newMsg->lastMsg = msgNum;
+        newMsg->msg = *content;
+        msgs->push_back(newMsg);
+
+        acknowledge(sock, content, msgNum, clientAddr);
+    } else if(msgNum = msg->lastMsg + 1) {
+        // append message
+        msg->msg.append(*content);
+        msg->lastMsg = msgNum;
+
+        acknowledge(sock, content, msgNum, clientAddr);
+    } else if(msgNum <= msg->lastMsg) {
+        // re-acknowledge
+        acknowledge(sock, content, msgNum, clientAddr);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    const int RECVMAX = 1400;       // longest message to receive
     int sock;                       // socket
     unsigned short csPort;          // client-server port
-    struct sockaddr_in localAddr;   // local address
+    struct sockaddr_in serverAddr;  // local address
 
-
-    // check format
+    // check parameters
     if(argc != 2) {
-        fprintf(stderr, "Usage: %s <UDP SERVER PORT> <SAVE FILE LOCATION>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <SERVER PORT> <SAVE FILE>\n", argv[0]);
         exit(1);
     }
 
@@ -34,38 +86,53 @@ int main(int argc, char *argv[]) {
     }
 
     // construct local address structure
-    memset(&localAddr, 0, sizeof(localAddr));    // zero out structure
-    localAddr.sin_family = AF_INET;                 // address family
-    localAddr.sin_addr.s_addr = htonl(INADDR_ANY);  // get local address
-    localAddr.sin_port = htons(csPort);             // port
+    memset(&serverAddr, 0, sizeof(serverAddr));   // zero out structure
+    serverAddr.sin_family = AF_INET;                 // address family
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);  // get local address
+    serverAddr.sin_port = htons(csPort);             // port
 
     // bind port to local address
-    if(bind(sock, (struct sockaddr *) &localAddr, sizeof(localAddr)) < 0) {
+    if(bind(sock, (struct sockaddr*) &serverAddr, sizeof(serverAddr)) < 0) {
         DieWithError("bind() failed");
     }
 
     printf("Port server is listening to is: %d\n", csPort);
 
-    struct sockaddr_in clientAddr;
-    unsigned int clientAddrLen;
-    char buffer[RECVMAX];
-    int rcvMsgSize;
+    struct sockaddr_in clientAddr;                          // client address
+    unsigned int clientAddrLen = sizeof(clientAddr);        // client address length
 
-    // always on server
-    #pragma clang diagnostic push
-    #pragma ide diagnostic ignored "EndlessLoop"
-    while(true) {
-        clientAddrLen = sizeof(clientAddr);
+    int rcvMsgSize;                                         // received message size
+    char buffer[RECVMAX];                                   // message buffer
 
-        if((rcvMsgSize = recvfrom(sock, buffer, RECVMAX, MSG_PEEK, (struct sockaddr*) &clientAddr, &clientAddrLen)) < 0) {
-            DieWithError("recvfrom() failed");
+    std::vector<message*> msgs;                             // collect messages
+    std::string buffStr;                                    // buffer as a string
+    std::string prefix;                                     // prefix command of message
+    int msgNum;                                             // identifier for message
+    std::string content;                                    // message content
+
+    // always on server loop
+    while(prefix != "EXT") {
+        // receive message
+        if((rcvMsgSize = recvfrom(sock, buffer, RECVMAX, 0, (struct sockaddr*) &clientAddr, &clientAddrLen)) < 0) {
+            continue;
         }
 
+        // check buffer
         buffer[rcvMsgSize] = '\0';
+        buffStr = std::string(buffer);
+        prefix = buffStr.substr(0, 3);
+        msgNum = stoi(buffStr.substr(3, buffStr.find(':', 3) - 3));
+        content = buffStr.substr(buffStr.find(':', 3) + 1);
 
-        printf("RCVFROM: %s - %s", inet_ntoa(clientAddr.sin_addr), buffer);
+        // check message type
+        if(prefix == "MSG") {   // construct message
+            handleMessage(sock, &msgs, &clientAddr, msgNum, &content);
+        } else if(prefix == "ACK") { // receive acknowledgement
+
+        } else {
+            printf("Received invalid message");
+        }
     }
-    #pragma clang diagnostic pop
 
     // close server
     close(sock);
